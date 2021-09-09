13ef292bc6468c18ca07336c126cc22a0f5911ab
Alexander Svistun <idoluum03@gmail.com>
2021-09-09 12:07:17 +0300

    Создал в класс BankMenu в нём 2 приватных метода: showLogin() - вызывается, если пользователь выбрал 1 showRegister() - вызывается, если пользователь выбрал 2

--- Bank/src/main/java/Bank.java
+++ Bank/src/main/java/Bank.java
@@ -1,5 +1,5 @@
 public class Bank {
     public static void start () {
-
+        BankMenu.showStartMenu();
     }
 }
---
+++ Bank/src/main/java/BankMenu.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class BankMenu {
+
+    public static void showStartMenu (){
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        System.out.println("""
+               Select one:
+               1. Login
+               2. Register
+               0. Exit""");
+            while (true) {
+                command = scanner.next();
+                switch (command) {
+                    case "1" -> showLogin();
+                    case "2" -> showRegister();
+                    case "0" -> System.exit(0);
+                    default -> System.out.println("Repeat one more time");
+                }
+            }
+
+    }
+    private static void showLogin () {
+        System.out.println("showLogin");
+
+    }
+    private static void showRegister () {
+        System.out.println("showRegister");
+
+    }
+}
